d6db3cdd19156866d7d82d948e3a4f855d94499b
Ardi Sahputra <ardysyahputra01@gmail.com>
2023-05-30 14:37:58 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+class User extends Authenticatable
+
 {
     use HasFactory, Notifiable;
 
