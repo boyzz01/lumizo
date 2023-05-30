54b115e48cb9eac621646399083aa2de1fa15677
Ardi Sahputra <ardysyahputra01@gmail.com>
2023-05-30 13:34:59 +0700

    a

--- config/mail.php
+++ config/mail.php
@@ -4,7 +4,7 @@ return [
     'driver' => env('MAIL_DRIVER', 'smtp'),
     'host' => env('MAIL_HOST', 'smtp.gmail.com'),
     'port' => env('MAIL_PORT', 587),
-    'from' => ['address' => 'karangtarunamdn@gmail.com', 'name' => 'Karang Taruna'],
+    'from' => ['address' => 'lunnizom.indoproperti@gmail.com', 'name' => 'Lunnizom'],
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
     'username' => env('MAIL_USERNAME'),
     'password' => env('MAIL_PASSWORD'),
--- resources/views/verifyEmail.blade.php
+++ resources/views/verifyEmail.blade.php
@@ -5,7 +5,7 @@
     <p>Dear {{ $user->name }}</p>
     <p>Akun Anda Berhasil Dibuat, Mohon aktivasi menggunakan link dibawah ini</p>
     <p><a href="{{ route('verify', $user->verification_token) }}">
-
+            {{ route('verify', $user->verification_token) }}
         </a></p>
     <p>Thanks</p>
 
