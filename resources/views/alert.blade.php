4f26de5614a611f4ea1b82c4cdd2851624f03cf9
Ardi Sahputra <ardysyahputra01@gmail.com>
2023-06-22 13:18:30 +0700

    Update alert.blade.php

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -15,6 +15,12 @@
     </div>
 @endif
 
+@if (session('error'))
+    <div class="alert alert-danger alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong> {{ session('error') }}</strong>
+    </div>
+@endif
 
 
 <script>
