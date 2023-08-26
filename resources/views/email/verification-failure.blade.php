27692c98513146f03d80152be8adc033e745cea4
Ardi Sahputra <ardysyahputra01@gmail.com>
2023-08-26 10:32:49 +0700

    Create verification-failure.blade.php

---
+++ resources/views/email/verification-failure.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gagal Verifikasi Email</title>
+</head>
+
+<body>
+    <h1>Gagal Verifikasi Email</h1>
+    <p>Link Ini tidak berfungsi atau sudah diaktivasi. Silahkan hubungin Support.</p>
+</body>
+
+</html>
