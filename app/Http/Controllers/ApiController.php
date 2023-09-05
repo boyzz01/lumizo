dde5ce1ee30f47c76e96d85d2cb10ce62532f3d5
Ardi Sahputra <ardysyahputra01@gmail.com>
2023-09-06 06:14:15 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -69,7 +69,15 @@ class ApiController extends Controller
                    ->where('pw_token', $request->otp)
                    ->first();
 
-       return response()->json('Kode OTP salah'.$request->email, 400);
+        if ($user) {
+            $user->password = bcrypt($request->password);
+            $user->pw_token = null; // Hapus OTP
+            $user->save();
+
+            return response()->json('Password berhasil direset');
+        } else {
+            return response()->json('Kode OTP salah', 400);
+        }
     }
 
     public function getSponsor()
